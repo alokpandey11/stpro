8bd352ef843ddb6e1bdc3cc9cb395086d2a7ae1b
ALOK PANDEY <alokpandey1131@gmail.com>
2021-09-23 08:21:26 +0530

    copyright correction

--- OnlinePizzaDelivery/about.php
+++ OnlinePizzaDelivery/about.php
@@ -59,7 +59,7 @@
             <div class="carousel-background"><img src="assets/img/slide/slide-3.jpg" alt=""></div>
             <div class="carousel-container">
               <div class="carousel-content">
-                <h2 class="animate__animated animate__fadeInDown mb-0">Parmar Darshan Kiritbhai</h2><p>CE084 <a href="https://github.com/darshankparmar" target="_blank">@darshankparmar</a></p>
+                <h2 class="animate__animated animate__fadeInDown mb-0">Alok Pandey</h2><p>CE084 <a href="https://github.com/alokpandey11" target="_blank">@alokpandey</a></p>
                 <a href="index.php" class="btn-get-started animate__animated animate__fadeInUp scrollto">Get Started</a>
               </div>
             </div>
@@ -198,12 +198,12 @@
               <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
               <div class="member-info">
                 <div class="member-info-content">
-                  <h4>Darshan Parmar</h4>
+                  <h4>Alok Pandey</h4>
                 </div>
                 <div class="social">
-                  <a href="https://twitter.com/darshankparmar" target="_blank"><i class="icofont-twitter"></i></a>
-                  <a href="https://github.com/darshankparmar" target="_blank"><i class="fab fa-github"></i></a>
-                  <a href="https://www.linkedin.com/in/darshankparmar/" target="_blank"><i class="icofont-linkedin" target="_blank"></i></a>
+                  <a href="https://twitter.com/alokpandey" target="_blank"><i class="icofont-twitter"></i></a>
+                  <a href="https://github.com/alokpandey11" target="_blank"><i class="fab fa-github"></i></a>
+                  <a href="https://www.linkedin.com/in/alokpandey11" target="_blank"><i class="icofont-linkedin" target="_blank"></i></a>
                 </div>
               </div>
             </div>
